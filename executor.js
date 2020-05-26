5155a91660d5b6226d9968add81243da50e2c573
Marcos Pereira <marcospereiradev@gmail.com>
2020-05-26 12:03:50 +0100

    Update executor.js

--- executor.js
+++ executor.js
@@ -25,47 +25,102 @@ const checkColorParameter = (color) => {
 const { train, test } = require("./00d62c1b.json");
 const grid = test[0].input;
 
+/**
+ * Create an object (array of coordinates) starting from a single square,
+ * by grouping it with all squares of non background color that are connected
+ * to it - either directly or through other non background color squares.
+ */
+const createObjectFromSquare = (y, x, grid, backgroundColor) => {
+  // Recursive function
+  // The parameter obj contains the points belonging to the object being created
+  const f = obj => {
+    // For each point in obj, check if there are any neighboring non background
+    // squares to be added to the object.
+    // If any are added, call itself recursively to keep expanding.
+    // If none are added, consider the object complete and return it.
+
+    let added = false;
+
+    for (const [y, x] of obj) {
+      const neighbors = [
+        [y, x + 1],
+        [y, x - 1],
+        [y + 1, x],
+        [y - 1, x],
+        [y + 1, x + 1],
+        [y + 1, x - 1],
+        [y - 1, x + 1],
+        [y - 1, x - 1],
+      ];
+
+      for (const [y2, x2] of neighbors) {
+        const alreadyInObject = obj.find(p => p.y === y2 && p.x === x2);
+        if (alreadyInObject !== undefined) continue;
+
+        if (grid[y2][x2] !== backgroundColor) {
+          obj.push([y2, x2]);
+          added = true;
+        }
+      }
+    }
+
+    if (added) {
+      return f(obj);
+    }
+    return obj;
+  }
+
+  return f([[y,x]]);
+}
+
 // Given a selector object, returns targeted coordinates of the grid
 const select = selector => {
   // Background color is black by default
   const backgroundColor = 0;
 
-  // an array of arrays containing coordinates of objects in the format [y, x]:
-  // [ [[0, 1], [0, 2]], ... ]
+  // an array of arrays containing objects -> coordinates in the format:
+  // [ [[y, x], [y, x]], ... ]
   let objects = [];
 
-  // Create initial objects array, holding coordinates of all objects of non
-  // background color in the grid
+  // Create initial objects array, which holds the coordinates of all objects
+  // of non background color in the grid
   for (const [y, row] of grid.entries()) {
     for (const [x, color] of row.entries()) {
 
       if (color !== backgroundColor) {
 
-        // If we have an object neighboring this square, add the square to it
-        let assigned = false;
-        for (const [i, coords] of objects.entries()) {
+        // This square is not of background color. If it does not belong to an
+        // object yet, form a new object from it by recursively grouping it with
+        // all non background color squares that are connected to it, either
+        // directly or through other squares of non background color.
+
+        let alreadyAssigned = false;
+
+        for (const coords of objects) {
           for (const [y2, x2] of coords) {
-
-            if (Math.abs(y - y2) <= 1 && Math.abs(x - x2) <= 1) {
-              // Add this square to neighboring object
-              objects[i].push([y, x]);
-
-              // Square has been assigned to an object, stop looking for objects
-              // to assign it to
-              assigned = true;
+            if (y === y2 && x === x2) {
+              alreadyAssigned = true;
               break;
             }
           }
-          if (assigned) break;
+          if (alreadyAssigned) break;
         }
 
-        // Otherwise, create a new object
-        if (!assigned) {
-          objects.push([[y, x]])
+        if (!alreadyAssigned) {
+          objects.push(createObjectFromSquare(y, x, grid, backgroundColor));
         }
       }
     }
   }
+
+  // Creating initial objects by iterating square by square is imperfect, as
+  // squares neighboring squares that haven't been checked yet will be seen as
+  // a new object.
+  // This means now we merge objects that neighbor each other:
+
+  // If there were any merges, check again as there might be new neighbors
+
+  console.log(`Found ${objects.length} objects`);
 
   // selector.part: only supports "interior" for now
   if (selector.part) {
@@ -74,9 +129,6 @@ const select = selector => {
     }
 
     if (selector.part === "interior") {
-
-      console.log("Select interiors");
-
       // Create one empty interior per object in selection
       const interiors = objects.map(x => []);
 
@@ -130,10 +182,12 @@ const color = (selector, color) => {
 
   const colorIndex = colors[color];
 
-  const coordinates = select(selector);
+  const selectedObjects = select(selector);
 
-  for (const [y, x] of coordinates) {
-    grid[y][x] = colorIndex;
+  for (const coords of selectedObjects) {
+    for (const [y, x] of coords) {
+      grid[y][x] = colorIndex;
+    }
   }
 }
 
